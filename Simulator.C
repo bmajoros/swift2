cf2577f1a7fa6e5f4166d287bb3c47cf75a5057e
William Majoros <bmajoros@dcc-allenlab-01.rc.duke.edu>
2023-09-28 15:49:53 -0400

    update

--- Simulator.C
+++ Simulator.C
@@ -13,7 +13,58 @@ using namespace BOOM;
 void Simulator::sim(const Experiment &realData,const int numNulls,
 		    Array1D<Experiment> &nulls)
 {
+  // Prepare output and precompute some things
+  const int numDnaReps=realData.DNA.size(), numRnaReps=realData.RNA.size();
   computeSums(realData);
+  prepareBetas(realData.DNA);
+  nulls.resize(numNulls);
+  for(int i=0 ; i<numNulls ; ++i) {
+    Experiment &null=nulls[i];
+    null.DNA.resize(numDnaReps);
+    null.RNA.resize(numRnaReps);
+  }
+
+  // Perform simulation
+  GSL::GslBinomial binom();
+  for(int i=0 ; i<numNulls ; ++i) {
+    // First, sample a value for p from the real DNA (using a beta distr)
+    // and use it to simulate new DNA allele counts
+    Experiment &null=nulls[i];
+    Vector<float> Ps;
+    for(int j=0 ; j<numDnaReps ; ++j) {
+      const float p=betas[j].random();
+      Ps.push_back(p);
+      binom.change(p);
+      const n=dnaSums[j];
+      const a=binom.random(n);
+      const b=n-a;
+      Replicate &dnaRep=null.DNA[j];
+      dnaRep.setRef(a); dnaRep.setAlt(b);
+    }
+    
+    // Assuming q=p, simulate RNA counts from a binomial
+    int nextP=0;
+    for(int j=0 ; j<numRnaReps ; ++j) {
+      const float q=Ps[nextP];
+      nextP=(nextP+1)%Ps.size();
+      binom.change(q);
+      const n=rnaSums[j];
+      const k=binom.random(n);
+      const m=n-k;
+      Replicate &rnaRep=null.RNA[j];
+      rnaRep.setRef(k); rnaRep.setAlt(m);
+    }
+  }
+}
+
+
+
+void Simulator::prepareBetas(const Replicates &DNA)
+{
+  const int numReps=DNA.size();
+  betas.resize(numReps);
+  for(int i=0 ; i<numReps ; ++i)
+    betas[i].change(DNA[i].getRef()+1,DNA[i].getAlt()+1);
 }
 
 
